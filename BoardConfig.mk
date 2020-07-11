05a9627334b6088b39d51975623aa0cd95654c2c
TALU <taluemail272@gmail.com>
2020-07-11 13:32:58 +0100

    BoardConfig: set TARGET_KERNEL_SOURCE.

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -55,6 +55,7 @@ TARGET_KERNEL_ARCH := arm64
 TARGET_KERNEL_HEADER_ARCH := arm64
 TARGET_KERNEL_CROSS_COMPILE_PREFIX := aarch64-linux-android-
 TARGET_KERNEL_CONFIG := kitakami_sumire_defconfig
+TARGET_KERNEL_SOURCE := kernel/sony/msm8994
 
 # Kernel configurations
 BOARD_KERNEL_BASE := 0x00000000
